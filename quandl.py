faf96f0033087841545e3ab7956c409effa1fe15
Junhwan Choi <choi.junhwan@gmail.com>
2016-03-09 17:16:48 -0600

    update quandl.py

--- quandl.py
+++ quandl.py
@@ -1,30 +1,31 @@
-import Quandl 
+import requests
+import simplejson as json
 import numpy as np
 from bokeh.plotting import figure, show, output_file, vplot
 
-def datetime(x):
-    return np.array(x, dtype=np.datetime64)
 
-stock_name = "YHOO"
-#stock_name = "GOOG"
-stock_call  = "WIKI/"+stock_name
+def stock_plot(stock, key):
+    MyAPIKey = "95ujF6cdywLzzj9uPSeS"
+    api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json?api_key=%s' % (stock, MyAPIKey)
+    json_data = requests.get(api_url)
 
-key_calls =["Open", "High", "Low", "Close", "Adj. Open", "Adj. High", "Adj. Low", "Adj. Close"]
-key_names =["Opening", "High", "Low", "Closing", "Adjust Opening", "Adjust High", "Adjust Low", "Adjust Closing"]
-index = key_calls.index("Adj. Open")
-key_call   = key_calls[index]
-key_name   = key_names[index]
+    #print json_data.json()['column_names']
+    data_list =  zip(*json_data.json()['data'])
+    key_calls = [x.encode('UTF8') for x in json_data.json()['column_names']]
+    index = key_calls.index(key)
+    date = np.array(list(data_list[0]), dtype=np.datetime64)
+    in_data = np.asarray(data_list[index])
 
-in_data = Quandl.get(stock_call, collapse="yearly")
+    p = figure(x_axis_type = "datetime")
+    p.title = "Stock Price Data from Quandl"
+    p.grid.grid_line_alpha=0.3
+    p.xaxis.axis_label = 'Date'
+    p.yaxis.axis_label = key + ' Price [$]'
+    
+    p.line(date, in_data, line_color='blue', line_width=2, legend='%s' % stock)
+    output_file("stocks.html", title="Quandl")
+    show(p)  # open a browser
 
-#print in_data.dtypes
 
-p = figure(x_axis_type = "datetime")
-p.title = "Stock Price Data from Quandl"
-p.grid.grid_line_alpha=0.3
-p.xaxis.axis_label = 'Date'
-p.yaxis.axis_label = key_name + ' Price [$]'
-
-p.line(datetime(in_data[key_call].index), in_data[key_call], line_color='blue', line_width=2, legend='%s' % stock_name)
-output_file("stocks.html", title="Quandl")
-show(p)  # open a browser
+if __name__=="__main__":    
+    stock_plot("YHOO","Open")
